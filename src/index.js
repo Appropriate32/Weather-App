6975537ae67fdb5ee5d3677cda0a0d15f53ab751
Appropriate32 <eissadar3218@gmail.com>
2026-01-29 23:52:59 +0500

    Made an application logic class and getWeather func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+
+class ApplicationLogic {
+  async getWeather(location) {
+    try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days&key=JZRVWS8M3H5SBPLUD9B8F3TZW&options=usefcst&contentType=json`,
+      );
+
+      if (!response.ok) {
+        throw new Error(`City not found (Status: ${response.status})`);
+      }
+      const data = await response.json();
+      console.log(data);
+      console.log(`Your address is ${data.address}`);
+      console.log(`Current temperature: ${data.days[0].temp}`);
+    } catch (error) {
+      console.error("Error fetching weather:", error);
+      return null;
+    }
+  }
+}
+
+const appLogic = new ApplicationLogic();
+
+const locationInput = prompt("What city?");
+
+appLogic.getWeather(locationInput);
