200f4243a0a0ef0715dde88fcd4f18ecac6db2d8
Appropriate32 <eissadar3218@gmail.com>
2026-01-30 21:42:50 +0500

    Made DOM class with dynamic displaying of API data on DOM

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ class ApplicationLogic {
         throw new Error(`City not found (Status: ${response.status})`);
       }
       const data = await response.json();
-      console.log(data);
-      console.log(`Your address is ${data.address}`);
-      console.log(`Current temperature: ${data.days[0].temp}`);
+      return data;
     } catch (error) {
       console.error("Error fetching weather:", error);
       return null;
@@ -21,8 +19,86 @@ class ApplicationLogic {
   }
 }
 
+class DisplayController {
+  constructor() {
+    this.forecastContainer = document.querySelector(".forecast");
+    this.tempInfo = document.querySelector(".temp-info");
+    this.locationHeading = document.querySelector(".current-location");
+    this.eventContainer = document.querySelector(".event-container");
+
+    this.inputSearch = document.querySelector(".search-location");
+    this.searchForm = document.querySelector(".search-bar form");
+
+    this.init();
+  }
+
+  init() {
+    if (this.searchForm) {
+      this.searchForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("Hello");
+
+        const city = this.getLocation();
+        appLogic.getWeather(city).then((response) => {
+          if (response) {
+            console.log(response);
+            displayLogic.displayTemperature(response);
+            displayLogic.displayForecast(response);
+          }
+        });
+      });
+    }
+  }
+
+  getLocation() {
+    return this.inputSearch.value;
+  }
+
+  displayTemperature(data) {
+    this.locationHeading.textContent = data.address;
+    this.tempInfo.innerHTML = `<p>Icon</p>
+        <p class="temp">${data.days[0].temp}</p>
+        <div class="temp-types">
+          <p class="active">C</p>
+          <p>F</p>
+        </div>`;
+    this.eventContainer.innerHTML = `<p class="weather-event">${data.days[0].icon}</p>
+        <p class="updated-time">Updated as of 11:23 AM</p>
+        <div class="weather-info">
+          <p>Feels like ${data.days[0].feelslike}°</p>
+          <p>Wind ${data.days[0].windspeed} km/h</p>
+          <p>${data.days[0].visibility} 4 km</p>
+        </div>`;
+  }
+
+  displayForecast(data) {
+    this.forecastContainer.textContent = "";
+
+    const next7Days = data.days.slice(1, 8);
+
+    next7Days.forEach((day) => {
+      const card = document.createElement("div");
+      card.classList.add("forecast-item");
+
+      card.innerHTML = `<div class="card-header">${this.formatDate(day.datetime)}</div>
+          <div class="card-body">
+            <p>Icon</p>
+            <div class="card-temps">
+              <p>${Math.round(day.tempmax)}°</p>
+              <p>${Math.round(day.tempmin)}°</p>
+            </div>
+            <p class="forecast-event">Storms</p>
+          </div>`;
+      this.forecastContainer.appendChild(card);
+    });
+  }
+
+  formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { weekday: "short", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  }
+}
+
 const appLogic = new ApplicationLogic();
-
-const locationInput = prompt("What city?");
-
-appLogic.getWeather(locationInput);
+const displayLogic = new DisplayController();
